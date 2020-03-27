83ef6d887b8f60dbd20ec26dc2ecfba75549b7b9
justinsorensen2dotnet <>
2020-03-27 00:30:37 -0400

    Completed home page - no styhling.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Featured from '../components/Featured'
+import TvShow from '../components/TvShow'
 
 const HomePage = () => {
-  return <HelloWorld />
+  const [tvShows, setTvShows] = useState([])
+
+  const getTopRated = async () => {
+    const resp = await axios.get(
+      'https://api.themoviedb.org/3/tv/top_rated?api_key=16dccf4635fc67bb4bf1e38c0a4459dd&language=en-US&page=1'
+    )
+    console.log(resp.data.results)
+    setTvShows(resp.data.results)
+  }
+
+  useEffect(() => {
+    console.log('did mount')
+    getTopRated()
+  }, [])
+
+  return (
+    <div>
+      <Featured></Featured>
+      <ul className="TopRated">
+        {tvShows.map((tvShow) => {
+          return (
+            <TvShow
+              key={tvShow.id}
+              name={tvShow.name}
+              overview={tvShow.overview}
+              poster_path={tvShow.poster_path}
+            />
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default HomePage
--- src/pages/TvShowPage.jsx
+++ src/pages/TvShowPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Page = () => {
+const TvShowPage = () => {
   return <div>This is a Page</div>
 }
 
-export default Page
+export default TvShowPage
